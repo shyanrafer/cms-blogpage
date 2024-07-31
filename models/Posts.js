3ce84065b623220c9df126f6d1cee54b0925f508
The Goose <ryanshaffer1324@gmail.com>
2024-07-31 19:21:36 -0400

    Added content to Posts model

--- models/Posts.js
+++ models/Posts.js
@@ -1,2 +1,39 @@
 // need id and post itself which will be a string
 // need to reference user id
+
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Posts extends Model {}
+
+Posts.init (
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    postBody: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'user',
+        key: 'id'
+      }
+    }
+  },
+  {
+    sequelize,
+    // setting timestamps to true
+    timestamps: true,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'posts',
+  }
+);
+
+module.exports = Posts;
